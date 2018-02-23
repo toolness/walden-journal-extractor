290b42998de6c0cb772eaa0a6ab997a2cd8e14eb
Atul Varma <varmaa@gmail.com>
2018-02-23 14:48:27 -0500

    Add example remote code

--- src/tsx/index.tsx
+++ src/tsx/index.tsx
@@ -1,4 +1,6 @@
 import { h, Component, render } from 'preact';
+import { remote } from 'electron';
+import { default as SaveGameType } from '../savegame';
 
 export interface AppProps {
     msg: string;
@@ -14,3 +16,9 @@ export class App extends Component<AppProps, AppState> {
 }
 
 render(<App msg="Hello."></App>, document.getElementById('app'));
+
+const SaveGame = remote.require('./dist/savegame').default as typeof SaveGameType;
+
+SaveGame.retrieveAll('bop').catch((e) => {
+    console.log('error', e);
+});
