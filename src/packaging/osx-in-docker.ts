6f548426984496be7d0643b0ec7e92e48ac899b8
Atul Varma <varmaa@gmail.com>
2018-03-04 17:17:30 -0500

    Quietly build zip file.

--- src/packaging/osx-in-docker.ts
+++ src/packaging/osx-in-docker.ts
@@ -13,14 +13,21 @@ const absReleaseDir = path.join(APP_DIR, releaseDir);
 
 runSync([
     `mkdir -p ${downloadCacheDir}`,
+
     `echo "Copying repository to staging directory..."`,
+
     `rm -rf ${appStagingDir}`,
     `cp -R ${APP_DIR}/ ${appStagingDir}`,
     `cd ${appStagingDir}`,
+
     `echo "Packaging app for OS X..."`,
+
     `npm run package-osx -- --download.cache=${downloadCacheDir}`,
-    `cd "release-builds/${productName}-darwin-x64"`,
-    `zip -y -r ${zipfile} "${productName}.app"`,
+
+    `echo "Creating ZIP archive..."`,
+
+    `cd "${releaseDir}/${productName}-darwin-x64"`,
+    `zip -y -r -q ${zipfile} "${productName}.app"`,
     `mkdir -p ${absReleaseDir}`,
     `cp ${zipfile} ${absReleaseDir}`,
     `echo "The packaged OS X app is in ${releaseDir}/${zipfile}."`
